21a8fe0f170f14fb74ea1f86a93614d728690327
Geeta <geeta.appu98@gmail.com>
2026-02-21 16:25:28 +0000

    dev

--- Dev/vite.config.js
+++ Dev/vite.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import  federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
     federation({
-      name :"Dev",
-      fileName:"remoteEntry.js",
-      expose:{
-        "./Dev":"./src/App.jsx"
+      name: "dev",
+      filename: "remoteEntry.js",
+      expose: {
+        "./Dev": "./src/App.jsx",
       },
-      shared:["react","react-dom"]
-    })
+      shared: ["react", "react-dom"],
+    }),
   ],
-  server:{
-    port:3001
-  }
-})
+  server: {
+    port: 3001,
+    cors: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+  },
+});
